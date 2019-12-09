a56d2eda980f6ee9b79463fd3847d1f9f38a9069
Kushan Vora <kushpvo@gmail.com>
2019-12-09 15:32:31 +0530

    Changed urls to support custom domain

--- short/admin.py
+++ short/admin.py
@@ -8,7 +8,7 @@ class LinkAdmin(admin.ModelAdmin):
     list_display = ['long_url', 'short_url_display']
 
     def short_url_display(self, instance):
-        return ("http://localhost:8000/" + str(instance.short_url()))
+        return ("https://huff.pw/" + str(instance.short_url()))
 
 
 admin.site.register(Link, LinkAdmin)
